10e7b73cd5c0bde5f7dbe8015b859274c93b5f1b
Dat Pham <dat.pham@ubitec.com>
2019-05-22 11:48:43 +0700

    implement routes for kubon app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+
 import App from "./components/init/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Users from "./components/users/users.js";
+import Contact from "./components/contact/contact.js";
+
+const routing = (
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
